3a77bf4eb0cc9c7894c73690d65b0859064732ae
Your Actual Name <Your Actual Email>
2016-03-03 17:12:54 -0700

    Functional database and Swiss Pairings software, assumes even number of players.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -4,26 +4,60 @@
 #
 
 import psycopg2
+import bleach
 
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
     return psycopg2.connect("dbname=tournament")
+    
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    db = connect()
+    c = db.cursor()
+    query = "delete from matches;"
+    c.execute(query)
+    db.commit()
 
+    query2 = "update Players as p set Games_Played = m.match_count, Score = w.Win_Count from public.Player_Match_Count as m, public.Player_Win_Count as w where p.P_ID = m.P_ID and p.P_ID = w.P_ID;"
+    c.execute(query2)
+    db.commit()
+    db.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    db = connect()
+    c = db.cursor()
+    query = "delete from players;"
+    c.execute(query)
+    db.commit()
+    db.close()
+
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    db = connect()
+    c = db.cursor()
+    query = "select count(*) from players;"
+    c.execute(query)
+    rows = c.fetchall()
+    count = int(str(rows[0][0]))
+    db.close()
+    return count
 
 
 def registerPlayer(name):
+    bleach.clean(name)
+    db = connect()
+    c = db.cursor()
+    query = "insert into Players(Name, Games_Played, Score) VALUES(%s,0,0 );"
+    c.execute(query, (name,))
+    db.commit()
+    db.close()
+
     """Adds a player to the tournament database.
   
     The database assigns a unique serial id number for the player.  (This
@@ -35,6 +69,14 @@ def registerPlayer(name):
 
 
 def playerStandings():
+    db = connect()
+    c = db.cursor()
+    query = "select p_id, name, score, games_played from players order by score;" 
+    c.execute(query)
+    rows = c.fetchall()
+    db.close()
+    return rows
+
     """Returns a list of the players and their win records, sorted by wins.
 
     The first entry in the list should be the player in first place, or a player
@@ -50,6 +92,19 @@ def playerStandings():
 
 
 def reportMatch(winner, loser):
+    bleach.clean(winner)
+    bleach.clean(loser)
+    db = connect()
+    c = db.cursor()
+    query = "insert into Matches(Round, P1, P2, Winner) Values(1,%d, %d, %d);" % (winner, loser, winner) 
+    c.execute(query)
+    db.commit()
+
+    query2 = "update Players as p set Games_Played = m.match_count, Score = w.Win_Count from public.Player_Match_Count as m, public.Player_Win_Count as w where p.P_ID = m.P_ID and p.P_ID = w.P_ID;"
+    c.execute(query2)
+    db.commit()
+
+    db.close()
     """Records the outcome of a single match between two players.
 
     Args:
@@ -57,8 +112,23 @@ def reportMatch(winner, loser):
       loser:  the id number of the player who lost
     """
  
- 
 def swissPairings():
+    db = connect()
+    c = db.cursor()
+    query = "select P_ID, Name, Score from players;"
+    c.execute(query)
+    rows = c.fetchall()
+    wincounts =sorted(rows, key=lambda tup: tup[2], reverse = True)
+
+    from itertools import groupby
+    groups = [list(g) for k,g in groupby(wincounts, lambda x: x[2])]
+
+    pairs = [(i[j][0], i[j][1], i[j+1][0], i[j+1][1]) for i in groups for j in range(0,len(i),2)]
+
+    return pairs
+
+
+
     """Returns a list of pairs of players for the next round of a match.
   
     Assuming that there are an even number of players registered, each player
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,4 +1,52 @@
 -- Table definitions for the tournament project.
+
+DROP DATABASE IF EXISTS tournament;
+
+CREATE DATABASE tournament;
+\c tournament;
+
+
+--1 Create Players table, where each player has the following information
+    --Unique ID - serial
+    --Name - string
+    --Number of games played - integer
+    --Running Score - integer
+
+CREATE TABLE Players(
+  P_ID serial PRIMARY KEY,
+  Name text,
+  Games_Played int,
+  Score int
+);
+
+
+
+--2 Create Matches table, where each game has the following info:
+  --Match Unique ID -serial
+  --Round Number - integer
+  --Player 1 - serial
+  --Player 2 - serial
+  --Winner - serial
+  --Draw - Boolean
+
+CREATE TABLE Matches(
+  M_ID serial PRIMARY KEY,
+  Round int,
+  P1 serial,
+  P2 serial,
+  Winner serial
+);
+
+Create View Player_Match_Count as (select public.players.P_ID, count(public.matches.P1 + public.matches.P2) as Match_Count from public.matches right outer join public.players on (public.matches.P1 = public.players.P_ID or public.matches.P2 = public.players.P_ID) group by public.players.P_ID);
+
+Create View Player_Win_Count as (select public.players.P_ID, count(public.matches.winner) as Win_Count from public.matches right outer join public.players on (public.matches.winner = public.players.P_ID) group by public.players.P_ID);
+
+--3 Create Rounds table, showing all player matchings that take place in a round:
+  --Round
+  --Game
+  --Winner
+
+
 --
 -- Put your SQL 'create table' statements in this file; also 'create view'
 -- statements if you choose to use it.
